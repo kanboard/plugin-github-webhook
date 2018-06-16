ec24d76f2ff5a6a34cce64138754440646ba2bc6
Balázs Úr <urbalazs@gmail.com>
2018-06-16 17:52:44 +0200

    Updated Hungarian translation

--- Locale/hu_HU/translations.php
+++ Locale/hu_HU/translations.php
@@ -1,16 +1,16 @@
 <?php
 
 return array(
-    'Github commit received' => 'Github commit érkezett',
-    'Github issue opened' => 'Github issue nyitás',
-    'Github issue closed' => 'Github issue zárás',
-    'Github issue reopened' => 'Github issue újranyitva',
-    'Github issue assignee change' => 'Github issue felelős változás',
-    'Github issue label change' => 'Github issue címke változás',
-    'Github webhooks' => 'Github webhooks',
-    'Help on Github webhooks' => 'Github Webhook súgó',
-    'Github issue comment created' => 'Github issue megjegyzés létrehozva',
-    'Github Issue' => 'Github issue',
-    // 'Commit made by @%s on Github' => '',
+    'Github commit received' => 'Github kommit érkezett',
+    'Github issue opened' => 'Github probléma megnyitva',
+    'Github issue closed' => 'Github probléma lezárva',
+    'Github issue reopened' => 'Github probléma újranyitva',
+    'Github issue assignee change' => 'Github problémához hozzárendelt személy megváltoztatása',
+    'Github issue label change' => 'Github probléma címke megváltoztatása',
+    'Github webhooks' => 'Github webhorgok',
+    'Help on Github webhooks' => 'Súgó a Github webhorgokhoz',
+    'Github issue comment created' => 'Github probléma megjegyzés létrehozva',
+    'Github Issue' => 'Github probléma',
+    'Commit made by @%s on Github' => '@%s által készített kommit a Githubon',
 );
 
